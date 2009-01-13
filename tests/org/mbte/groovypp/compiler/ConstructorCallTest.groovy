56db281e987fdcc768bca1a5fe08c3fb81967b20
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-13 17:56:45 +0000

    initial commit

---
+++ tests/org/mbte/groovypp/compiler/ConstructorCallTest.groovy
@@ -0,0 +1,20 @@
+package org.mbte.groovypp.compiler
+
+public class ConstructorCallTest extends GroovyShellTestCase {
+
+    void testNoParam () {
+
+    }
+
+    void testNoParamButMap () {
+
+    }
+
+    void testThis () {
+
+    }
+
+    void testSuper () {
+
+    }
+}
