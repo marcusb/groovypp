33cfa624d604fb23720b6e8f8bc2d04ea7d96208
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-13 17:57:04 +0000

    initial commit

---
+++ tests/org/mbte/groovypp/compiler/GStringTest.groovy
@@ -0,0 +1,33 @@
+package org.mbte.groovypp.compiler
+
+
+public class GStringTest extends GroovyShellTestCase {
+
+    void testCounter () {
+        def res = shell.evaluate("""
+class Counter {
+  public int value
+
+  Counter next () {
+    value++
+    this
+  }
+
+  String toString () { value.toString() }
+}
+
+@CompileStatic
+def m () {
+  def counter = new Counter ()
+  def gs = "counter \$counter"
+
+  while (!(counter.value == 5)) {
+    counter++
+    println gs
+  }
+}
+
+m ()
+        """)
+    }
+}
