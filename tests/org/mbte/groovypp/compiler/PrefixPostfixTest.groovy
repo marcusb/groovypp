3d4185ceb7f7b4a25159d93925f140e3379663c5
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-13 17:58:26 +0000

    initial commit

---
+++ tests/org/mbte/groovypp/compiler/PrefixPostfixTest.groovy
@@ -0,0 +1,195 @@
+package org.mbte.groovypp.compiler
+
+public class PrefixPostfixTest extends GroovyShellTestCase {
+    void testPostfixVar() {
+        ["int", "Integer", "byte", "Byte",
+                "short", "Short", "long", "Long", "float", "Float", "double", "Double",
+                "char", "Character"].each {
+
+            println it
+
+            def res = shell.evaluate("""
+          @CompileStatic
+          def u ($it val, List res) {
+            while(val) {
+              res.add(val--)
+            }
+            res
+          }
+
+            u (($it)5,[])
+            """
+            )
+
+            println res
+            assertEquals([5, 4, 3, 2, 1], res.collect {jt -> (int) jt })
+        }
+    }
+
+    void testPostfixProp() {
+        ["", "static"].each { st ->
+            ["int", "Integer", "byte", "Byte",
+                    "short", "Short", "long", "Long", "float", "Float", "double", "Double",
+                    "char", "Character"].each {
+
+                println it
+
+                def res = shell.evaluate("""
+              class U {
+                public $st $it prop
+              }
+
+              @CompileStatic
+              def u (U u, List res) {
+                while(u.prop) {
+                  res.add(u.prop--)
+                }
+                res
+              }
+
+                u (new U(prop:($it)5),[])
+                """
+                )
+
+                println res
+                assertEquals([5, 4, 3, 2, 1], res.collect {jt -> (int) jt })
+            }
+        }
+    }
+
+    void testPrefixProp() {
+        ["", "static"].each { st ->
+            ["int", "Integer", "byte", "Byte",
+                    "short", "Short", "long", "Long", "float", "Float", "double", "Double",
+                    "char", "Character"].each {
+
+                println it
+
+                def res = shell.evaluate("""
+            import org.mbte.groovygrid.compile.*
+              class U {
+                public $st $it prop
+              }
+
+              @CompileStatic
+              def u (U u, List res) {
+                while(u.prop) {
+                  res.add(--u.prop)
+                }
+                res
+              }
+
+                u (new U(prop:($it)5),[])
+                """
+                )
+
+                println res
+                assertEquals([4, 3, 2, 1, 0], res.collect {jt -> (int) jt })
+            }
+        }
+    }
+
+    void testPostfixArr() {
+        ["int", "Integer", "byte", "Byte",
+                "short", "Short", "long", "Long", "float", "Float", "double", "Double",
+                "char", "Character"].each {
+
+            println it
+
+            def res = shell.evaluate("""
+    @CompileStatic
+    def U ($it[] val, List res) {
+      while(val[0]) {
+        res.add(val[0]--)
+      }
+      res
+    }
+
+      U ([($it)5] as $it[],[])
+      """
+            )
+
+            println res
+            assertEquals([5, 4, 3, 2, 1], res.collect {jt -> (int) jt })
+        }
+    }
+
+    void testPrefixVar() {
+        ["int", "Integer", "byte", "Byte",
+                "short", "Short", "long", "Long", "float", "Float", "double", "Double",
+                "char", "Character"].each {
+
+            println it
+
+            def res = shell.evaluate("""
+          @CompileStatic
+          def u ($it val, List res) {
+            while(val) {
+              res.add(--val)
+            }
+            res
+          }
+
+            u (($it)5,[])
+            """
+            )
+
+            println res
+            assertEquals([4, 3, 2, 1, 0], res.collect {jt -> (int) jt })
+        }
+    }
+
+    void testPrefixArr() {
+        ["int", "Integer", "byte", "Byte",
+                "short", "Short", "long", "Long", "float", "Float", "double", "Double",
+                "char", "Character"].each {
+
+            println it
+
+            def res = shell.evaluate("""
+      @CompileStatic
+      def U ($it[] val, List res) {
+        while(val[0]) {
+          res.add(--val[0])
+        }
+        res
+      }
+
+        U ([($it)5] as $it[],[])
+        """
+            )
+
+            println res
+            assertEquals([4, 3, 2, 1, 0], res.collect {jt -> (int) jt })
+        }
+    }
+
+    void testVar() {
+        def types = ["int", "Integer", "byte", "Byte",
+                "short", "Short", "long", "Long", "float", "Float", "double", "Double",
+                "char", "Character", "BigInteger", "BigInteger"]
+
+        types.each { jt ->
+        types.each { it ->
+            println "$it, $jt"
+
+            def res = shell.evaluate("""
+      @CompileStatic
+      def U (List res) {
+        $it val = ($jt)5
+        while(val) {
+          res.add(--val)
+        }
+        res.collect{ kt -> (int) kt }
+      }
+
+        U ([])
+        """
+            )
+
+            println res
+            assertEquals([4, 3, 2, 1, 0], res)
+        }
+        }
+    }
+}
