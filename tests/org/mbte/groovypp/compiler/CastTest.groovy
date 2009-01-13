fb02ad0886f4e4de23217b511c2f2f4250f3a280
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-13 17:56:04 +0000

    initial commit

---
+++ tests/org/mbte/groovypp/compiler/CastTest.groovy
@@ -0,0 +1,82 @@
+package org.mbte.groovypp.compiler
+
+public class CastTest extends GroovyShellTestCase {
+
+    void testCoerceObject () {
+        def res = shell.evaluate("""
+@CompileStatic
+def m () {
+  [1,2,3] as Set
+}
+
+m()
+        """)
+
+        assertTrue res instanceof Set
+    }
+
+    void testCoercePrimitive () {
+        def res = shell.evaluate("""
+@CompileStatic
+def m () {
+  1 as Long
+}
+
+m()
+        """)
+        assertTrue res instanceof Long
+    }
+
+    void testNumber () {
+        def res = shell.evaluate("""
+@CompileStatic
+def m () {
+  (Number)0
+}
+
+m()
+        """)
+        assertTrue res instanceof Integer
+    }
+
+    void testNoCoerce () {
+        def res = shell.evaluate("""
+@CompileStatic
+def m () {
+  def u = (Serializable)[:]
+  u.put("k","v")
+  u
+}
+
+m()
+        """)
+        assertEquals ([k:"v"], res) 
+    }
+
+    void testPrimitiveNoCoerce () {
+        def res = shell.evaluate("""
+@CompileStatic
+def m () {
+  Object u = (byte)3;
+  [(long)1, (int)u]
+}
+
+m()
+        """)
+        assertEquals ([1L, 3], res) 
+
+    }
+
+    void testNoCoerceWithInference () {
+        def res = shell.evaluate("""
+@CompileStatic
+def m () {
+  def u = (byte)3;
+  [(long)1, (int)u]
+}
+
+m()
+        """)
+        assertEquals ([1L, 3], res)
+    }
+}
