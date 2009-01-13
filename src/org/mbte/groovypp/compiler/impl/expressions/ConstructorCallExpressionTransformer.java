01a85cc3107f10b04c7fa00b6a65b0a6035e6e0c
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-13 17:44:57 +0000

    initial commit

---
+++ src/org/mbte/groovypp/compiler/impl/expressions/ConstructorCallExpressionTransformer.java
@@ -0,0 +1,21 @@
+package org.mbte.groovypp.compiler.impl.expressions;
+
+import org.codehaus.groovy.ast.expr.ConstructorCallExpression;
+import org.codehaus.groovy.ast.expr.Expression;
+import org.codehaus.groovy.classgen.BytecodeHelper;
+import org.mbte.groovypp.compiler.impl.CompilerTransformer;
+import org.mbte.groovypp.compiler.impl.BytecodeExpr;
+
+public class ConstructorCallExpressionTransformer extends ExprTransformer<ConstructorCallExpression>{
+
+    public Expression transform(ConstructorCallExpression exp, CompilerTransformer compiler) {
+        return new BytecodeExpr(exp, exp.getType()) {
+            protected void compile() {
+                final String classInternalName = BytecodeHelper.getClassInternalName(getType());
+                mv.visitTypeInsn(NEW, classInternalName);
+                mv.visitInsn(DUP);
+                mv.visitMethodInsn(INVOKESPECIAL, classInternalName, "<init>", "()V");
+            }
+        };
+    }
+}
