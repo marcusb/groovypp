1a1a12ad792c503c1ff49fbedb6bc2d7a6a37f67
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-13 17:48:25 +0000

    initial commit

---
+++ src/org/mbte/groovypp/compiler/impl/expressions/SpreadExpressionTransformer.java
@@ -0,0 +1,18 @@
+package org.mbte.groovypp.compiler.impl.expressions;
+
+import org.codehaus.groovy.ast.expr.Expression;
+import org.codehaus.groovy.ast.expr.SpreadExpression;
+import org.mbte.groovypp.compiler.impl.*;
+
+public class SpreadExpressionTransformer extends ExprTransformer<SpreadExpression>{
+    public Expression transform(SpreadExpression exp, CompilerTransformer compiler) {
+        final BytecodeExpr internal = (BytecodeExpr) compiler.transform(exp.getExpression());
+
+        if (!TypeUtil.isDirectlyAssignableFrom(TypeUtil.COLLECTION_TYPE, internal.getType())) {
+          compiler.addError("Spread operator can be applied only to java.util.Collection", exp);
+          return null;
+        }
+
+        return new BytecodeSpreadExpr(exp, internal);
+    }
+}
