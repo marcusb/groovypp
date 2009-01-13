1b3a523f2ed4ff8d6df80ace7da3b0fb012e6ec2
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-13 17:55:43 +0000

    initial commit

---
+++ tests/org/mbte/groovypp/compiler/AssignTest.groovy
@@ -0,0 +1,28 @@
+package org.mbte.groovypp.compiler
+
+public class AssignTest extends GroovyShellTestCase {
+
+    void testVarAssign () {
+
+    }
+
+    void testArrayAssign () {
+
+    }
+
+    void testArrayAssignViaProperty () {
+
+    }
+
+    void testAssignProperty () {
+
+    }
+
+    void testAssignFinalVar () {
+
+    }
+
+    void testAssignFinalField () {
+
+    }
+}
