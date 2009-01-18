1da98af6c54d43cc6013db447dc2800400faca0c
Alex Tkachman <alex.tkachman@gmail.com>
2009-01-18 20:00:13 +0000

    some bugs

--- src/org/mbte/groovypp/compiler/transformers/ClosureExpressionTransformer.java
+++ src/org/mbte/groovypp/compiler/transformers/ClosureExpressionTransformer.java
@@ -1,9 +1,9 @@
-package org.mbte.groovypp.compiler.expressions;
+package org.mbte.groovypp.compiler.transformers;
 
 import org.codehaus.groovy.ast.expr.ClosureExpression;
 import org.codehaus.groovy.ast.expr.Expression;
-import org.mbte.groovypp.compiler.CompilerTransformer;
 import org.mbte.groovypp.compiler.CompiledClosureBytecodeExpr;
+import org.mbte.groovypp.compiler.CompilerTransformer;
 
 public class ClosureExpressionTransformer extends ExprTransformer<ClosureExpression> {
     public Expression transform(ClosureExpression exp, CompilerTransformer compiler) {
