04c611778a1f8c5644ad00868ee6ceaa7240ac41
Alex Tkachman <alex.tkachman@gmail.com>
2009-09-21 12:07:09 +0000

    support for synchronized

--- tests/org/mbte/groovypp/compiler/SyncTest.groovy
+++ tests/org/mbte/groovypp/compiler/SyncTest.groovy
@@ -6,15 +6,17 @@ public class SyncTest extends GroovyShellTestCase {
     @Compile
     def u () {
       def o = new Object ();
+      def exception = false
       try {
         synchronized (o) {
            assert !Thread.currentThread ().holdsLock(o)
         }
       }
       catch (Throwable t) {
+           exception = true
            assert !Thread.currentThread ().holdsLock(o)
       }
-      assert !Thread.currentThread ().holdsLock(o)
+      assert exception && !Thread.currentThread ().holdsLock(o)
     }
     u ()
     """
