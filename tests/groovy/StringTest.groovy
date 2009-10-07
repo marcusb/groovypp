dafc7e81dcff4f7e61eee40ec62f8de021578323
Juiry Bura <juriy.bura@gmail.com>
2009-10-07 15:57:17 +0000

    String test added

--- tests/groovy/StringTest.groovy
+++ tests/groovy/StringTest.groovy
@@ -210,164 +210,230 @@ y''' == 'x\\ny'
         }
         u()
       """;
-      println script;
       shell.evaluate script;
     }
-/*
-    void testBoolCoerce() {
-        // Explicit coercion
-        assertFalse((Boolean) "")
-        assertTrue((Boolean) "content")
 
-        // Implicit coercion in statements
-        String s = null
-        if (s) {
-            fail("null should have evaluated to false, but didn't")
-        }
-        s = ''
-        if (s) {
-            fail("'' should have evaluated to false, but didn't")
-        }
-        s = 'something'
-        if (!s) {
-            fail("'something' should have evaluated to false, but didn't")
-        }
-        
+    void testBoolCoerce() {
+        shell.evaluate  """
+
+          @Typed
+          def u() {
+            // Explicit coercion
+            assert !((Boolean) "")
+            assert ((Boolean) "content")
+
+            // Implicit coercion in statements
+            String s = null
+            if (s) {
+                assert false
+            }
+            s = ''
+            if (s) {
+                 assert false
+            }
+            s = 'something'
+            if (!s) {
+                 assert false
+            }
+          }
+
+          u()
+        """
+
     }
 
     void testSplit() {
-        def text = "hello there\nhow are you"
-        def splitted = text.split().toList()
-        assert splitted == ['hello', 'there', 'how', 'are', 'you']
+      shell.evaluate  """
+
+        @Typed
+        def u() {
+          def text = "hello there\\nhow are you"
+          def splitted = text.split()
+          assert splitted == ['hello', 'there', 'how', 'are', 'you']
+        }
+
+        u()
+      """
+
     }
 
     void testSplitEmptyText() {
-        def text = ""
-        def splitted = text.split().toList()
-        assert splitted == []
+      shell.evaluate  """
+
+        @Typed
+        def u() {
+          def text = ""
+          def splitted = text.split()
+          assert splitted == []
+        }
+
+        u()
+      """
+
     }
 
     void testReadLines() {
-        assert "a\nb".readLines() == ['a', 'b']
-        assert "a\rb".readLines() == ['a', 'b']
-        assert "a\r\nb".readLines() == ['a', 'b']
-        assert "a\n\nb".readLines() == ['a', '', 'b']
+      shell.evaluate  """
+
+        @Typed
+        def u() {
+          assert "a\\nb".readLines() == ['a', 'b']
+          assert "a\\rb".readLines() == ['a', 'b']
+          assert "a\\r\\nb".readLines() == ['a', 'b']
+          assert "a\\n\\nb".readLines() == ['a', '', 'b']
+        }
+
+        u()
+      """
     }
     
     void testReplace() {
-        assert "".replace("", "") == ""
-        assert "".replace("", "r") == "r"
-        assert "a".replace("", "r") == "rar"
-        assert "a".replace("b", "c") == "a"
-        assert "a".replace("a", "c") == "c"
-        assert "aa".replace("a", "c") == "cc"
-        assert "ab".replace("b", "c") == "ac"
-        assert "ba".replace("b", "c") == "ca"
-        assert "aaa".replace("b", "c") == "aaa"
-        assert "aaa".replace("a", "c") == "ccc"
-        assert "aba".replace("b", "c") == "aca"
-        assert "baa".replace("b", "c") == "caa"
-        assert "aab".replace("b", "c") == "aac"
-        assert "aa.".replace(".", "c") == "aac"
-        assert 'aba'.replace('b', '$') == 'a$a'
-        assert 'aba'.replace('b', '\\') == 'a\\a'
-        assert 'a\\a'.replace('\\', 'x') == 'axa'
-        assert '\\'.replace('\\', 'x') == 'x'
-        assert '\\\\'.replace('\\', 'x') == 'xx'
-        assert '\\z\\'.replace('\\', 'x') == 'xzx'
-        assert 'a\\\\Ea'.replace('\\', 'x') == 'axxEa'
-        assert '\\Qa\\\\Ea'.replace('\\', '$') == '$Qa$$Ea'
-        assert 'a\\((z))\\Qa'.replace('\\', 'x') == 'ax((z))xQa'
-        assert (/\Q\E\\\Q\E/).replace(/\Q\E\\\Q\E/, 'z') == 'z'
+      shell.evaluate  """
+
+        @Typed
+        def u() {
+          assert "".replace("", "") == ""
+          assert "".replace("", "r") == "r"
+          assert "a".replace("", "r") == "rar"
+          assert "a".replace("b", "c") == "a"
+          assert "a".replace("a", "c") == "c"
+          assert "aa".replace("a", "c") == "cc"
+          assert "ab".replace("b", "c") == "ac"
+          assert "ba".replace("b", "c") == "ca"
+          assert "aaa".replace("b", "c") == "aaa"
+          assert "aaa".replace("a", "c") == "ccc"
+          assert "aba".replace("b", "c") == "aca"
+          assert "baa".replace("b", "c") == "caa"
+          assert "aab".replace("b", "c") == "aac"
+          assert "aa.".replace(".", "c") == "aac"
+          assert 'aba'.replace('b', '\$') == 'a\$a'
+          assert 'aba'.replace('b', '\\\\') == 'a\\\\a'
+          assert 'a\\\\a'.replace('\\\\', 'x') == 'axa'
+
+          assert '\\\\'.replace('\\\\', 'x') == 'x'
+          assert '\\\\\\\\'.replace('\\\\', 'x') == 'xx'
+          assert '\\\\z\\\\'.replace('\\\\', 'x') == 'xzx'
+          assert 'a\\\\\\\\Ea'.replace('\\\\', 'x') == 'axxEa'
+          assert '\\\\Qa\\\\\\\\Ea'.replace('\\\\', '\$') == '\$Qa\$\$Ea'
+          assert 'a\\\\((z))\\\\Qa'.replace('\\\\', 'x') == 'ax((z))xQa'
+
+          assert (/\\Q\\E\\\\\\Q\\E/).replace(/\\Q\\E\\\\\\Q\\E/, 'z') == 'z'
+        }
+        u()
+      """
+
     }
 
     void testNormalize() {
-        assert "a".normalize() == "a"
-        assert "\n".normalize() == "\n"
-        assert "\r".normalize() == "\n"
-        assert "\r\n".normalize() == "\n"
-        assert "a\n".normalize() == "a\n"
-        assert "a\r".normalize() == "a\n"
-        assert "a\r\n".normalize() == "a\n"
-        assert "a\r\n\r".normalize() == "a\n\n"
-        assert "a\r\n\r\n".normalize() == "a\n\n"
-        assert "a\nb\rc\r\nd".normalize() == "a\nb\nc\nd"
-        assert "a\n\nb".normalize() == "a\n\nb"
-        assert "a\n\r\nb".normalize() == "a\n\nb"
+         shell.evaluate  """
+
+          @Typed
+          def u() {
+            assert "a".normalize() == "a"
+            assert "\\n".normalize() == "\\n"
+            assert "\\r".normalize() == "\\n"
+            assert "\\r\\n".normalize() == "\\n"
+            assert "a\\n".normalize() == "a\\n"
+            assert "a\\r".normalize() == "a\\n"
+            assert "a\\r\\n".normalize() == "a\\n"
+            assert "a\\r\\n\\r".normalize() == "a\\n\\n"
+            assert "a\\r\\n\\r\\n".normalize() == "a\\n\\n"
+            assert "a\\nb\\rc\\r\\nd".normalize() == "a\\nb\\nc\\nd"
+            assert "a\\n\\nb".normalize() == "a\\n\\nb"
+            assert "a\\n\\r\\nb".normalize() == "a\\n\\nb"
+          }
+
+          u()
+        """
     }
 
     void testDenormalize() {
-        def LS = System.getProperty('line.separator')
-        assert "\n".denormalize() == LS
-        assert "\r".denormalize() == LS
-        assert "\r\n".denormalize() == LS
-        assert "a\n".denormalize() == "a${LS}"
-        assert "a\r".denormalize() == "a${LS}"
-        assert "a\r\n".denormalize() == "a${LS}"
-        assert "a\r\n\r".denormalize() == "a${LS}${LS}"
-        assert "a\r\n\r\n".denormalize() == "a${LS}${LS}"
-        assert "a\nb\rc\r\nd".denormalize() == "a${LS}b${LS}c${LS}d"
-        assert "a\n\nb".denormalize() == "a${LS}${LS}b"
-        assert "a\n\r\nb".denormalize() == "a${LS}${LS}b"
-        assert 'a\nb\r\nc\n\rd'.denormalize() == "a${LS}b${LS}c${LS}${LS}d"
-    }
-    
-    void innerNormalizationFileRoundTrip(String s) {
-        def f = File.createTempFile("groovy.StringTest", ".txt")
-        
-        def sd = s.denormalize()
-        f.write(sd)
-        assert sd == f.text
-        
-        f.write(s); 
-        assert s == f.text
-        
-        def rt = (s.denormalize()).normalize()
-        assert s.normalize() == rt
+        shell.evaluate  """
 
-        if (!s.contains('\r')) assert s == rt
-    }
+          @Typed
+          def u() {
+            def LS = System.getProperty('line.separator')
+            assert "\\n".denormalize() == LS
+            assert "\\r".denormalize() == LS
+            assert "\\r\\n".denormalize() == LS
+            assert "a\\n".denormalize() == "a\${LS}"
+            assert "a\\r".denormalize() == "a\${LS}"
+            assert "a\\r\\n".denormalize() == "a\${LS}"
+            assert "a\\r\\n\\r".denormalize() == "a\${LS}\${LS}"
+            assert "a\\r\\n\\r\\n".denormalize() == "a\${LS}\${LS}"
+            assert "a\\nb\\rc\\r\\nd".denormalize() == "a\${LS}b\${LS}c\${LS}d"
+            assert "a\\n\\nb".denormalize() == "a\${LS}\${LS}b"
+            assert "a\\n\\r\\nb".denormalize() == "a\${LS}\${LS}b"
+            assert 'a\\nb\\r\\nc\\n\\rd'.denormalize() == "a\${LS}b\${LS}c\${LS}\${LS}d"
+          }
 
-    void doNormalizationFileRoundTrip(String s) {
-        [s, s.replace('\n', '\r'), s.replace('\n', '\r\n'), s.replace('\n', '\n\n')].each {
-            innerNormalizationFileRoundTrip(it)
-            innerNormalizationFileRoundTrip(it.reverse())
-        }
+          u()
+        """
     }
 
     void testNormalizationFileRoundTrip() {
-        doNormalizationFileRoundTrip("a line 1\nline 2")
-        doNormalizationFileRoundTrip("a line 1\nline 2\n")
-        doNormalizationFileRoundTrip("")
-        doNormalizationFileRoundTrip("\n")
-        doNormalizationFileRoundTrip("a")
-        doNormalizationFileRoundTrip("abcdef")
-        doNormalizationFileRoundTrip("a\n")
-        doNormalizationFileRoundTrip("abcdef\n")
-    }
+      shell.evaluate  """
+        @Typed
+        void innerNormalizationFileRoundTrip(String s) {
+            def f = File.createTempFile("groovy.StringTest", ".txt")
+
+            def sd = s.denormalize()
+            f.write(sd)
+            assert sd == f.text
+
+            f.write(s);
+            assert s == f.text
+
+            def rt = (s.denormalize()).normalize()
+            assert s.normalize() == rt
+
+            if (!s.contains('\\r')) assert s == rt
+        }
+
+        @Typed
+        void doNormalizationFileRoundTrip(String s) {
+            
+            def arr = [s, s.replace('\\n', '\\r'), s.replace('\\n', '\\r\\n'), s.replace('\\n', '\\n\\n')];
+            arr.each {
+                innerNormalizationFileRoundTrip((String)it)
+                innerNormalizationFileRoundTrip(it.reverse())
+            }
+        }
+
+        @Typed
+        def u() {
+          doNormalizationFileRoundTrip("a line 1\\nline 2")
+          doNormalizationFileRoundTrip("a line 1\\nline 2\\n")
+          doNormalizationFileRoundTrip("")
+          doNormalizationFileRoundTrip("\\n")
+          doNormalizationFileRoundTrip("a")
+          doNormalizationFileRoundTrip("abcdef")
+          doNormalizationFileRoundTrip("a\\n")
+          doNormalizationFileRoundTrip("abcdef\\n")
+
+        }
+
+        u()
+      """
+   }
     
     void testSplitEqualsTokenize() {
-        def text = """
-        A text with different words and
-        numbers like 3453 and 3,345,454.97 and
-        special characters %&)( and also
-        everything mixed together 45!kw?
-      """
+        shell.evaluate  """
 
-        def splitted = text.split().toList()
-        def tokenized = text.tokenize()
-        assert splitted == tokenized
-    }
+          @Typed
+          def u() {
+            def text = '''
+              A text with different words and
+              numbers like 3453 and 3,345,454.97 and
+              special characters %&)( and also
+              everything mixed together 45!kw?
+            '''
+            def splitted = Arrays.asList(text.split())
+            def tokenized = text.tokenize()
+            assert splitted == tokenized
 
-    private assertLength(s, len) {
-        if (s.length() != len)  println "*** length != $len: $s"
-        assert s.length() == len
+          }
+          u()
+        """
     }
-
-    private assertContains(s, len, subs) {
-        assertLength(s, len)
-        if (s.indexOf(subs) < 0)  println "*** missing $subs: $s"
-        assert s.indexOf(subs) >= 0
-    }
-*/
 }
