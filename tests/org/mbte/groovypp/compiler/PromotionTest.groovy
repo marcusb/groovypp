7dfbcbba9af8093b968b9c30e5582a872dbe08d5
Juiry Bura <juriy.bura@gmail.com>
2009-10-08 11:47:22 +0000

    Promotion breaks. Test added.

---
+++ tests/org/mbte/groovypp/compiler/PromotionTest.groovy
@@ -0,0 +1,44 @@
+package org.mbte.groovypp.compiler
+
+
+public class PromotionTest extends GroovyShellTestCase{
+  void testPromo1() {
+    shell.evaluate """
+    @Typed
+    def u () {
+      def C = '1'.toCharacter()
+      def B = new Byte("1")
+      assert (C + B instanceof Integer)
+    }
+    u ()
+    """
+  }
+
+  void testPromo2() {
+    shell.evaluate """
+    @Typed
+    def u () {
+      def C = '1'.toCharacter()
+      def BD = new BigDecimal("1.0")
+      assert (C - BD instanceof BigDecimal)
+    }
+    u ()
+    """
+  }
+
+
+  void testPromo3() {
+    shell.evaluate """
+    @Typed
+    def u () {
+      def C = '1'.toCharacter()
+      def B = new Byte("1")
+      def BD = new BigDecimal("1.0")
+
+      assert (C + B instanceof Integer)
+      assert (C - BD instanceof BigDecimal)
+    }
+    u ()
+    """
+  }
+}
