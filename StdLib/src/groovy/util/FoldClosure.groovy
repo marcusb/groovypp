7ec19f45244929de6e07766171f9f93f2be49a75
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2009-10-28 16:35:20 +0000

    cleanup

--- StdLib/src/groovy/util/FoldClosure.groovy
+++ StdLib/src/groovy/util/FoldClosure.groovy
@@ -9,32 +9,32 @@ abstract class FoldClosure<T1,T2,R> extends DefaultGroovyMethodsSupport {
   public static <T,R> R foldLeft(Collection<T> self, R init, FoldClosure<T,R,R> transform) {
     R res = init
     for (T t : self) {
-      res = transform.transform(t, res);
+      res = transform.transform(t, res)
     }
-    return res;
+    res
   }
 
   public static <T,R> R foldLeft(T[] self, R init, FoldClosure<T,R,R> transform) {
     R res = init
     for (T t : self) {
-      res = transform.transform(t, res);
+      res = transform.transform(t, res)
     }
-    return res;
+    res
   }
 
   public static <T,R> R foldRight(T[] self, R init, FoldClosure<T,R,R> transform) {
     R res = init
     for (int i = self.length - 1; i >= 0; --i) {
-      res = transform.transform(self[i], res);
+      res = transform.transform(self[i], res)
     }
-    return res;
+    res
   }
 
   public static <T,R> R foldRight(ArrayList<T> self, R init, FoldClosure<T,R,R> transform) {
     R res = init
     for (int i = self.size() - 1; i >= 0; --i) {
-      res = transform.transform(self.get(i), res);
+      res = transform.transform(self.get(i), res)
     }
-    return res;
+    res
   }
 }
