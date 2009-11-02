724848207a1a148eb3d80398c7e7829acc42b556
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2009-11-02 20:47:04 +0000

    idea inspections rule

--- StdLibTest/test/groovy/util/ClosureWithStateTest.groovy
+++ StdLibTest/test/groovy/util/ClosureWithStateTest.groovy
@@ -3,7 +3,7 @@ package groovy.util
 @Typed
 public class ClosureWithStateTest extends GroovyTestCase {
     void testMe() {
-      def sum = [1, 2, 3].each(0){int it, int sum -> sum += it}
+      def sum = [1, 2, 3].each(0){int it, int sum -> sum + it}
       assertEquals(6, sum)
     }
 }
