1e73eabb8c2edc37892c8e122140fe13504660df
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2009-11-03 16:03:39 +0000

    rename

--- StdLib/src/groovy/lang/Function0.groovy
+++ StdLib/src/groovy/lang/Function0.groovy
@@ -5,6 +5,6 @@ abstract class Function0<R> {
     abstract R call ()
 
     public <R1> Function0<R1> andThen (Function1<R,R1> g) {
-        { -> g.apply(apply()) }
+        { -> g.call(call()) }
     }
 }
--- StdLib/src/groovy/lang/Function1.groovy
+++ StdLib/src/groovy/lang/Function1.groovy
@@ -5,14 +5,14 @@ abstract class Function1<T,R> {
     abstract R call (T param)
 
     public <R1> Function1<T,R1> andThen (Function1<R,R1> g) {
-        { T arg -> g.apply(apply(arg)) }
+        { T arg -> g.call(call(arg)) }
     }
 
     public <T1> Function1<T1,R> composeWith (Function1<T1,T> g) {
-        { T arg -> apply(g.apply(arg)) }
+        { T arg -> call(g.call(arg)) }
     }
 
     R getAt (T arg) {
-        apply(arg)
+        call(arg)
     }
 }
