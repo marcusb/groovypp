cf50de2f00338cf7f79b974e84b7cac67f1dccca
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2009-11-11 11:35:31 +0000

    failing test

---
+++ tests/groovy/ForInTest.groovy
@@ -0,0 +1,21 @@
+package groovy
+
+import static groovy.CompileTestSupport.shouldCompile
+import static groovy.CompileTestSupport.shouldNotCompile
+
+class ForInTest extends GroovyShellTestCase {
+
+  void testForIn() {
+    def res = shell.evaluate("""
+      @Typed
+      public def foo(List<String> l) {
+        def res = []
+        for (el in l) res << el.toLowerCase()
+      }
+      foo(["Es", "GEHT"])
+    """)
+    assertEquals(["es", "geht"], res)
+  }
+
+
+}
