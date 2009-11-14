d3060e906c5ffca8cd0ce0ab472c460b028ae657
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2009-11-14 18:11:57 +0000

    correct (Integer)null == null cast.

--- tests/org/mbte/groovypp/compiler/FiltersTest.groovy
+++ tests/org/mbte/groovypp/compiler/FiltersTest.groovy
@@ -3,10 +3,12 @@ package org.mbte.groovypp.compiler
 public class FiltersTest extends GroovyShellTestCase {
   void testSimple() {
     assertTrue shell.evaluate("""
-        @Typed(debug=true)
+        @Typed
         class C extends GroovyTestCase {
           def test() {
             assertEquals([0,3], (0..5).filter{it % 3 == 0})
+            assertEquals(8, [1,2,3,5,8,13].find{it == 8})
+            assertNull([1,2,3,5,8,13].find{it == 9})
             true
           }
         }
