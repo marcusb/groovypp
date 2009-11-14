3f86e9e56b06fec897a0e94ee1a0907159e8edb8
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2009-11-14 20:17:35 +0000

    tests

--- tests/org/mbte/groovypp/compiler/FiltersTest.groovy
+++ tests/org/mbte/groovypp/compiler/FiltersTest.groovy
@@ -9,6 +9,8 @@ public class FiltersTest extends GroovyShellTestCase {
             assertEquals([0,3], (0..5).filter{it % 3 == 0})
             assertEquals(8, [1,2,3,5,8,13].find{it == 8})
             assertNull([1,2,3,5,8,13].find{it == 9})
+            assertEquals([2,8], [1,2,3,5,8,13].findAll{it % 2 == 0})
+            assertTrue(((int[])[23,10]).any{it.toString().contains("1")})
             true
           }
         }
