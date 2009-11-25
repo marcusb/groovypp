6c5d91e7123034ed285067a6fe41e6824c5ad056
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2009-11-25 10:21:17 +0000

    constructors can be @Typed.

--- src/groovy/lang/Typed.java
+++ src/groovy/lang/Typed.java
@@ -8,7 +8,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
-@Target({ElementType.METHOD, ElementType.TYPE, ElementType.PACKAGE})
+@Target({ElementType.METHOD, ElementType.CONSTRUCTOR, ElementType.TYPE, ElementType.PACKAGE})
 @GroovyASTTransformationClass("org.mbte.groovypp.compiler.CompileASTTransform")
 public @interface Typed {
     boolean debug() default false;
