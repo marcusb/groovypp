65b6547720f051cdc8378c0cc4842c510228a5a9
Alex Tkachman <alex.tkachman@gmail.com>
2009-12-27 16:18:53 +0000

    a bit better test

--- tests/org/mbte/groovypp/compiler/Issues/Issue18Test.groovy
+++ tests/org/mbte/groovypp/compiler/Issues/Issue18Test.groovy
@@ -3,11 +3,12 @@ package org.mbte.groovypp.compiler.Issues
 public class Issue18Test extends GroovyShellTestCase {
     void testFail () {
         shell.evaluate """
-            @Typed(debug=true) def u () {
+            @Typed def u () {
                 def j = 0
-                for (int i in (0..12).step(2)) {
+                for (int i in (0..4).step(2)) {
                    j += i
                 }
+                assert j == 6
             }
         """
     }
