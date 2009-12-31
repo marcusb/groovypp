6271ec201092a4531157915443884ea5a35c2fed
Alex Tkachman <alex.tkachman@gmail.com>
2009-12-31 09:03:34 +0000

    prime gen with caching

--- Examples/src/primenumbers/PrimeGenerator.groovy
+++ Examples/src/primenumbers/PrimeGenerator.groovy
@@ -3,50 +3,65 @@ package primenumbers
 import groovy.util.concurrent.FHashMap
 import java.util.concurrent.atomic.AtomicReference
 import groovy.xml.MarkupBuilder
-import java.util.concurrent.Executors
-import java.util.concurrent.ExecutorService
 import groovy.util.concurrent.FList
-import groovy.util.concurrent.Agent
+import java.util.concurrent.LinkedBlockingQueue
 
-@Typed FList<Integer> divisors(int n, FList<Integer> alreadyFound = FList.emptyList) {
-    if (n > 3)
-        for(candidate in 2..<n)
-           if (n % candidate == 0)
-                return divisors (n / candidate, alreadyFound + candidate)
+@Typed class PrimeCache {
+    final AtomicReference<FHashMap<Integer,Boolean>> primes = [FHashMap.emptyMap.put(2,true)]
 
-    alreadyFound + n
+    FList<Integer> divisors(int n, FList<Integer> alreadyFound = FList.emptyList) {
+        if (n > 2) {
+            if(!primes.get()[n]) {
+                for (p in primes.get()) {
+                    int candidate = p.key
+                    if (n % candidate == 0)
+                        return divisors (n.candidate, alreadyFound + candidate)
+                }
+
+                for(candidate in 2..<n)
+                   if (n % candidate == 0)
+                        return divisors (n / candidate, alreadyFound + candidate)
+            }
+        }
+
+        if (alreadyFound.empty)
+            primes.apply { p -> p.put(n,true) }
+
+        alreadyFound + n
+    }
 }
 
 @Typed(value=TypePolicy.MIXED)
 void generate () {
-    AtomicReference<FHashMap<Integer,Boolean>> primes = [FHashMap.emptyMap]
 
-    new MarkupBuilder().numbers {
-        ExecutorService pool = Executors.newFixedThreadPool(10)
+    def primeCache = new PrimeCache()
 
-        (2..1500).iterator ().mapConcurrently (pool, 50) {
-            def divs = divisors(it)
+    def queue = new LinkedBlockingQueue()
 
-            if (divs.size() == 1)
-                primes.apply { p -> p.put(it, true) }
-
-            [it, divs]
-        }.each { pair ->
-            if (pair [1].size () == 1)
-                number (value: pair[0], prime:true)
-            else {
-                number (value: pair[0], prime:false) {
-                  pair[1].reverse().each { div ->
-                    divisor(value: div)
-                  }
+    def builderProcess = Thread.start {
+        new MarkupBuilder().numbers {
+            for (List pair; pair = queue.take();) {
+                if (pair [1].size () == 1)
+                    number (value: pair[0], prime:true)
+                else {
+                    number (value: pair[0], prime:false) {
+                      pair[1].reverse().each { div ->
+                        divisor(value: div)
+                      }
+                    }
                 }
             }
         }
-
-        pool.shutdown()
     }
 
-    def keys = primes.get()*.key.sort()
+    for(n in 2..1500) {
+        queue << [n, primeCache.divisors(n)]
+    }
+    queue << []
+
+    builderProcess.join ()
+
+    def keys = primeCache.primes*.key.sort()
     println "\nNumber of primes: ${keys.size()}"
     for(k in keys) {
         println k
