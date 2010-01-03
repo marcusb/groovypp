01c404142a257652ff8fc6a7666570a8692123cf
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2010-01-03 09:43:36 +0000

    correct test.

--- tests/org/mbte/groovypp/compiler/Issues/Issue31Test.groovy
+++ tests/org/mbte/groovypp/compiler/Issues/Issue31Test.groovy
@@ -2,14 +2,16 @@ package org.mbte.groovypp.compiler.Issues
 
 public class Issue31Test extends GroovyShellTestCase {
     void testBug () {
-        shell.evaluate """
+        def res = shell.evaluate("""
             @Typed
-            void testFoldLeftWithMap () {
-                def res = [1,2,3,3,2,1].foldLeft (new HashMap<?,Integer>()) { el, map ->
+            def testFoldLeftWithMap () {
+                [1,2,3,3,2,1].foldLeft (new HashMap<?,Integer>()) { el, map ->
                     map[el] = map.get(el,0) + 1
+                    map
                 }
-                assertEquals res, [1:2, 2:2, 3:2]
             }
-        """
+            testFoldLeftWithMap()
+        """)
+        assertEquals res, [1:2, 2:2, 3:2]
     }
 }
