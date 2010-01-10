9bb1a147aa22061ef569c0d08dc5e24294e86c87
Alex Tkachman <alex.tkachman@gmail.com>
2010-01-10 13:50:54 +0000

    issue 39 - failing test

---
+++ tests/org/mbte/groovypp/compiler/Issues/Issue39Test.groovy
@@ -0,0 +1,13 @@
+package org.mbte.groovypp.compiler.Issues
+
+@Typed
+public class Issue39Test extends GroovyShellTestCase {
+    void testCompile () {
+        shell.evaluate """
+        @Typed def u () {
+            Iterable<Pair<Integer,Long>> arr = new LinkedList<Pair<Integer,Long>> ()
+            arr.toArray().sort{ a, b -> a.first <=> b.first }
+        }
+        """
+    }
+}
