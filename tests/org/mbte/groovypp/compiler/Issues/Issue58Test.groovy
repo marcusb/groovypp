6eafc4462ee64776af9da59896be122fe246d19e
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2010-01-24 09:26:44 +0000

    issue 58 test.

---
+++ tests/org/mbte/groovypp/compiler/Issues/Issue58Test.groovy
@@ -0,0 +1,11 @@
+package org.mbte.groovypp.compiler.Issues
+
+public class Issue58Test extends GroovyShellTestCase {
+    void testMe () {
+        shell.evaluate """
+        @Typed package p
+        def x = {int i -> if (i > 0) doCall(i -1) }
+        x(1000000)
+        """
+    }
+}
