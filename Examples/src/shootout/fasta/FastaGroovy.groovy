d2151df6cdffece60f44219bf0c9a530903e5b26
Alex Tkachman <alex.tkachman@gmail.com>
2010-03-18 07:05:35 +0000

    some speedup

--- Examples/src/shootout/fasta/FastaGroovy.groovy
+++ Examples/src/shootout/fasta/FastaGroovy.groovy
@@ -2,22 +2,22 @@ package shootout.fasta
 
 @Typed
 class FastaGroovy {
-	public static final int IM = 139968;
-	public static final int IA = 3877;
-	public static final int IC = 29573;
-	public static int last = 42;
+	static final int IM = 139968
+	static final int IA = 3877
+	static final int IC = 29573
+	static int last = 42
 
-	static int BUFFER_SIZE = 1024;
-	static int index = 0;
-	static byte[] bbuffer = new byte[BUFFER_SIZE];
+	static int BUFFER_SIZE = 1024
+	static int index = 0
+	static byte[] bbuffer = new byte[BUFFER_SIZE]
 	
 
-	public static final int LINE_LENGTH = 60;
+	static final int LINE_LENGTH = 60
 
 	// pseudo-random number generator
-	public static final double random(double max) {
-	    last = (last * IA + IC) % IM;
-	    return max * last / IM;
+	static final double random(double max) {
+	    last = (last * IA + IC) % IM
+	    max * last / IM
 	}
 
 	// Weighted selection from alphabet
@@ -28,36 +28,36 @@ class FastaGroovy {
 				"ACAAAAATTAGCCGGGCGTGGTGGCGCGCGCCTGTAATCCCA" +
 				"GCTACTCGGGAGGCTGAGGCAGGAGAATCGCTTGAACCCGGG" +
 				"AGGCGGAGGTTGCAGTGAGCCGAGATCGCGCCACTGCACTCC" +
-				"AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA";
+				"AGCCTGGGCGACAGAGCGAGACTCCGTCTCAAAAA"
 
-	static byte[] ALUB = ALU.getBytes();
+	static byte[] ALUB = ALU.bytes
 
 	static final Frequency[] IUB = [
-	        new Frequency((char)'a', 0.27),
-	        new Frequency((char)'c', 0.12),
-	        new Frequency((char)'g', 0.12),
-	        new Frequency((char)'t', 0.27),
+	        [(char)'a', 0.27d],
+	        [(char)'c', 0.12d],
+	        [(char)'g', 0.12d],
+	        [(char)'t', 0.27d],
 
-	        new Frequency((char)'B', 0.02),
-	        new Frequency((char)'D', 0.02),
-	        new Frequency((char)'H', 0.02),
-	        new Frequency((char)'K', 0.02),
-	        new Frequency((char)'M', 0.02),
-	        new Frequency((char)'N', 0.02),
-	        new Frequency((char)'R', 0.02),
-	        new Frequency((char)'S', 0.02),
-	        new Frequency((char)'V', 0.02),
-	        new Frequency((char)'W', 0.02),
-	        new Frequency((char)'Y', 0.02) ];
+	        [(char)'B', 0.02d],
+	        [(char)'D', 0.02d],
+	        [(char)'H', 0.02d],
+	        [(char)'K', 0.02d],
+	        [(char)'M', 0.02d],
+	        [(char)'N', 0.02d],
+	        [(char)'R', 0.02d],
+	        [(char)'S', 0.02d],
+	        [(char)'V', 0.02d],
+	        [(char)'W', 0.02d],
+	        [(char)'Y', 0.02d] ]
 
 	static final Frequency[] HomoSapiens = [
-	        new Frequency((char)'a', 0.3029549426680d),
-	        new Frequency((char)'c', 0.1979883004921d),
-	        new Frequency((char)'g', 0.1975473066391d),
-	        new Frequency((char)'t', 0.3015094502008d)];
+	        [(char)'a', 0.3029549426680d],
+	        [(char)'c', 0.1979883004921d],
+	        [(char)'g', 0.1975473066391d],
+	        [(char)'t', 0.3015094502008d]]
 
 	static void makeCumulative(Frequency[] a) {
-		double cp = 0.0;
+		double cp = 0
 		for (int i = 0; i < a.length; i++) {
 			cp += a[i].p;
 			a[i].p = cp;
@@ -67,32 +67,27 @@ class FastaGroovy {
 	// naive
 	final static byte selectRandom(Frequency[] a) {
 	    int len = a.length
-	    double r = random(1.0)
+	    double r = random(1.0d)
 	    for (int i = 0; i < len; i++)
 	        if (r < a[i].p)
 	            return a[i].c
 	    return a[len - 1].c
 	}
 
-	static final void makeRandomFasta(String id, String desc, Frequency[] a, int n, OutputStream writer)
-		throws IOException {
+	static final void makeRandomFasta(String id, String desc, Frequency[] a, int n, OutputStream writer) throws IOException {
 	    index = 0
 	    int m = 0
-	    String descStr = ">$id $desc \n"
-//	    writer.write(descStr.getBytes());
 	    while (n > 0) {
 	        if (n < LINE_LENGTH) m = n;  else m = LINE_LENGTH
 	        if(BUFFER_SIZE - index < m) {
-//	            writer.write(bbuffer, 0, index);
-	            index = 0;
+	            index = 0
 	        }
 	        for (int i = 0; i < m; i++) {
-	            bbuffer[index++] = selectRandom(a);
+	            bbuffer[index++] = selectRandom(a)
 	        }
 	        bbuffer[index++] = (char)'\n'
 	        n -= LINE_LENGTH;
 	    }
-//	    if(index != 0) writer.write(bbuffer, 0, index);
 	}
 
 	static final void makeRepeatFasta(String id, String desc, String alu, int n, OutputStream writer) throws IOException {
@@ -120,10 +115,10 @@ class FastaGroovy {
 	}
 
 	static void main(String[] args) {
-		long start = System.currentTimeMillis()
+		def start = System.currentTimeMillis()
 		makeCumulative(HomoSapiens)
 		makeCumulative(IUB)
-		int n = 25000
+		def n = 25000
 		if (args.length > 0)
 		    n = Integer.parseInt(args[0])
 		OutputStream out = System.out
