04e71efed7b0c5a32615408909115ee0fa73a384
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2010-03-26 09:52:13 +0000

    evaluate() -> parse() for compilation-related tests.

--- tests/groovy/CompileTestSupport.groovy
+++ tests/groovy/CompileTestSupport.groovy
@@ -3,35 +3,33 @@ package groovy
 import org.codehaus.groovy.control.CompilationFailedException
 import static org.junit.Assert.fail
 import org.codehaus.groovy.control.MultipleCompilationErrorsException
-import org.codehaus.groovy.syntax.SyntaxException;
+import org.codehaus.groovy.syntax.SyntaxException
 
 public class CompileTestSupport {
   static void shouldNotCompile(String script, String messageFragment = "") {
-    GroovyShell shell = new GroovyShell();
     try {
-      shell.evaluate (script)
+      new GroovyShell().parse(script)
     } catch (MultipleCompilationErrorsException e) {
       assert e.message.contains(messageFragment)
-      println e.errorCollector.errors[0].class 
+      println e.errorCollector.errors[0].class
       def error = e.errorCollector.errors[0].cause
       if (error instanceof SyntaxException) {
         assert error.startLine > 0 && error.startColumn > 0
       } else {
         assert error.line > 0 && error.column > 0
       }
-      return;
+      return
     } catch (CompilationFailedException e) {
-      fail("Unknown error occured while compiling");
+      fail("Unknown error occured while compiling")
     }
-    fail("Given code fragment should not compile");
+    fail("Given code fragment should not compile")
   }
 
   static void shouldCompile(String script) {
-    GroovyShell shell = new GroovyShell();
     try {
-      shell.evaluate (script)
+      new GroovyShell().parse(script)
     } catch (CompilationFailedException e) {
-      fail("Given code fragment should compile");
+      fail("Given code fragment should compile")
     }
   }
 
