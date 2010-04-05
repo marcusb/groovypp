4bb7bc26634d3197d156bc62db7cd91deb2d4491
Alex Tkachman <alex.tkachman@gmail.com>
2010-04-05 08:10:31 +0000

    attempt fix tests

--- src/org/mbte/groovypp/compiler/StructASTTransform.groovy
+++ src/org/mbte/groovypp/compiler/StructASTTransform.groovy
@@ -89,7 +89,7 @@ class StructASTTransform implements ASTTransformation, Opcodes {
             builderClassNode.addAnnotation(typedAnn)
 
             builderClassNode.genericsTypes = builderGenericTypes(classNode)
-            builderClassNode.unresolvedSuperClass = TypeUtil.withGenericTypes(superBuilder, builderSuperclassTypes(classNode, builderClassNode))
+            builderClassNode.superClass = TypeUtil.withGenericTypes(superBuilder, builderSuperclassTypes(classNode, builderClassNode))
 
             classNode.getModule().addClass(builderClassNode);
 
@@ -195,11 +195,8 @@ class StructASTTransform implements ASTTransformation, Opcodes {
         if (!gt)
             gt = new GenericsType[0]
 
-        def builderGt = new GenericsType[gt.length + 1]
-        ClassNode varT = ClassHelper.make("T")
-        varT.setRedirect(classNode)
-        varT.genericsPlaceHolder = true
-        builderGt[0] = new GenericsType(varT, [classNode], null)
+        def builderGt = new GenericsType[gt.length+1]
+        builderGt[0] = new GenericsType(classNode)
         for (i in 0..<gt.length)
             builderGt[i+1] = gt[i]
         return builderGt
