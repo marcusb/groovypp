3b037a55457dc1c7b0898920fd8fffad5c8b5fd5
Eugene Vigdorchik <eugene.vigdorchik@gmail.com>
2010-04-18 15:22:26 +0000

    proper fix.

--- src/org/mbte/groovypp/compiler/TypeUtil.java
+++ src/org/mbte/groovypp/compiler/TypeUtil.java
@@ -537,7 +537,7 @@ public class TypeUtil {
     }
 
     public static ClassNode withGenericTypes(ClassNode baseType, GenericsType[] genericTypes) {
-        if (ClassHelper.isPrimitiveType(baseType))
+        if (baseType.redirect().getGenericsTypes() == null)
             return baseType;
         
         ClassNode newBase = makeWithoutCaching(baseType.getName());
