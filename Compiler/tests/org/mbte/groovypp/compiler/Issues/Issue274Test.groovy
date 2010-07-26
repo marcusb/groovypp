88aa2630bcd949c0383e48b940485dda731dc73a
Alex Tkachman <alex.tkachman@gmail.com>
2010-07-26 10:35:35 +0000

    imports fixed

---
+++ Compiler/tests/org/mbte/groovypp/compiler/Issues/Issue274Test.groovy
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2009-2010 MBTE Sweden AB.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.mbte.groovypp.compiler.Issues
+
+@Typed
+public class Issue274Test extends GroovyShellTestCase {
+    void testMe () {
+       shell.evaluate """
+        @Typed package p
+
+def foo(String[] s){ s.length }
+String bar(String[] s){ s[0] }
+
+assert bar([]) == ''
+assert foo([]) == 1
+assert bar(['1234']).substring(1) == '234'
+assert foo('1234') == 1
+        """
+    }
+}
